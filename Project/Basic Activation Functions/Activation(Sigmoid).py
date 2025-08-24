4ad53a8f3031c8a72bfb0a6bd9be92f6085dd8f7
peeyush1278 <138887326+peeyush1278@users.noreply.github.com>
2025-08-24 13:25:20 +0530

    Update Activation(Sigmoid).py

--- Project/Basic Activation Functions/Activation(Sigmoid).py
+++ Project/Basic Activation Functions/Activation(Sigmoid).py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+random.seed(42)
 class activation_fnc:
     def __init__(self,y,n):
         self.y=y
@@ -8,7 +9,7 @@ class activation_fnc:
     def Active(self):
         result=np.zeros((1,self.n))
         for i in range(self.n):
-            result[0][i]=1/(1+math.exp(-(self.y[0][i])))
+            result[0][i]=1/(1+math.exp(-(self.y[i][0])))
         return result
 class results:
     def __init__(self,x,y):
@@ -30,8 +31,8 @@ if user_input.lower()=='yes':
                 break
             else:
                 count+=1
-                print("input node are there: ", ends=(""))
-                n=input()
+                print("input node are there: ")
+                n=int(input())
                 w=np.zeros((n,3))
                 for i in range(n):
                     w[i][0]=random.randint(-1,1)
@@ -57,4 +58,35 @@ if user_input.lower()=='yes':
                 Y_actual=random.randint(0,1)
                 loss_fnx=Y_actual-output_final
                 if loss_fnx>=0.1:
-                    pass                
+                    pass
+                                  
+        else:
+            count+=1
+            print("input node are there: ")
+            n=int(input())
+            w=np.zeros((n,3))
+            for i in range(n):
+                w[i][0]=random.randint(-1,1)
+                w[i][1]=random.randint(-1,1)
+                w[i][2]=random.randint(-1,1)
+            x=np.zeros((1,n))
+            for i in range(1):
+                x[i][0]=int(input("Enter the X value:"))                               
+                x[i][1]=int(input("Enter the X value:"))                               
+                x[i][2]=int(input("Enter the X value:"))   
+            y=np.zeros((n,1))
+            for i in range(n):
+                for j in range(n):
+                    y[i][0]+=x[0][j]*w[j][i]
+            act_fnc=activation_fnc(y,n)
+            result=act_fnc.Active()
+            W_y=np.zeros((1,3))
+            for i in range(3):
+                w[0][i]=random.randint(-1,1)
+            Y_final=results(result,W_y)
+            output=Y_final.outcome()
+            output_final=1/(1+math.exp(-(output)))
+            Y_actual=random.randint(0,1)
+            loss_fnx=Y_actual-output_final
+            if loss_fnx>=0.1:
+                pass
